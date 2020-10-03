28e0f66e3e8dcb7dbdc8b3cb696ca841aaeac7c7
TithimonPhusa <71490035+TithimonPhusa@users.noreply.github.com>
2020-10-03 16:06:44 +0700

    finish(?)

---
+++ Assets/Stage2ControlScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Stage2ControlScript : MonoBehaviour
+{
+    [SerializeField] Button _backButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackToStageSelectClick(Button button){
+        SceneManager.UnloadSceneAsync("Stage2");
+        SceneManager.LoadScene("StageSelect");
+    }
+}
---
+++ Assets/StageSelectControlScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageSelectControlScript : MonoBehaviour
+{
+
+    [SerializeField] Button _backButton;
+    [SerializeField] Button _stage1Button;
+    [SerializeField] Button _stage2Button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackToMainMenuButtonClick(Button button){
+        SceneManager.UnloadSceneAsync("StageSelect");
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ToStage1ButtonClick(Button button){
+        SceneManager.UnloadSceneAsync("StageSelect");
+        SceneManager.LoadScene("Stage1");
+    }
+    public void ToStage2ButtonClick(Button button){
+        SceneManager.UnloadSceneAsync("StageSelect");
+        SceneManager.LoadScene("Stage2");
+    }
+}
